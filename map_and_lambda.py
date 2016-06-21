5e6ae7b6addee03fcfb4c6ab40225811fe5d22c4
blue <bledikv@gmail.com>
2016-06-22 00:48:09 +0300

    Make minor changes

--- map_and_lambda.py
+++ map_and_lambda.py
@@ -9,16 +9,16 @@
 
 # >> print (list(map(len, ['Tina', 'Raj', 'Tom'])))  
 # [4, 3, 3]  
-# Lambda is a single expression anonymous function often used as an inline function. In simple words, it is a function that has only one line in 
-# its body. It proves very handy in functional and GUI programming.
+# Lambda is a single expression anonymous function often used as an inline function. In simple words, it is a function that has only one 
+# line in its body. It proves very handy in functional and GUI programming.
 
 # >> sum = lambda a, b, c: a + b + c
 # >> sum(1, 2, 3)
 # 6
 # Note:
 
-# Lambda functions cannot use the return statement and can only have a single expression. Unlike def, which creates a function and assigns it a name, 
-# lambda creates a function and returns the function itself. Lambda can be used inside lists and dictionaries.
+# Lambda functions cannot use the return statement and can only have a single expression. Unlike def, which creates a function and assigns 
+# it a name, lambda creates a function and returns the function itself. Lambda can be used inside lists and dictionaries.
 
 # Input Format
 

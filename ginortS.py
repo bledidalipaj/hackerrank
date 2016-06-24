2820f679fe1b13335492c682f290fdcb49187f16
blue <bledikv@gmail.com>
2016-06-25 00:32:25 +0300

    Solution for ginortS problem

---
+++ ginortS.py
@@ -0,0 +1,42 @@
+"""
+	You are given a string S. 
+	S contains alphanumeric characters only. 
+	Your task is to sort the string S in the following manner:
+
+	All sorted lowercase letters are ahead of uppercase letters.
+	All sorted uppercase letters are ahead of digits.
+	All sorted odd digits are ahead of sorted even digits.
+	Input Format
+
+	A single line of input contains the string S.
+
+	Constraints
+	0 < len(S) < 100
+
+
+	Output Format
+
+	Output the sorted string S.
+
+	Sample Input
+
+	Sorting1234
+	Sample Output
+
+	ginortS1324
+	Note: 
+	a) Using join, for or while anywhere in your code, even as substrings, will result in a score of 0. 
+	b) You can only use one sorted function in your code. A 0 score will be awarded for using sorted more than once. 
+
+	Hint: Success is not the key, but key is success.
+
+	Problem's link: https://www.hackerrank.com/challenges/ginorts/forum
+"""
+
+from __future__ import print_function
+
+S = raw_input().strip()
+
+order = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1357902468'
+sorted_S = sorted(S, key=order.index)
+print(*sorted_S, sep='')
